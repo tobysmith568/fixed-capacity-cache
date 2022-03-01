2863e90a9d3b57a9bed0a80431a8a185f04662a8
Toby Smith <tobysmith568@hotmail.co.uk>
2022-03-01 22:28:35 +0000

    Move the tests into a top-level directory

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,16 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+
+const { pathsToModuleNameMapper } = require("ts-jest/utils");
+const { compilerOptions } = require("./tsconfig");
+
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   globals: {
     "ts-jest": {
-      tsconfig: "tsconfig.test.json"
+      tsconfig: "tsconfig.json"
     }
-  }
+  },
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: "<rootDir>/" })
 };
--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,4 +1,4 @@
-import { FixedCapacityCache } from "../index";
+import { FixedCapacityCache } from "src/index";
 
 describe("FixedCapacityCache", () => {
   describe("constructor", () => {
